52d2e003e6ef1c48d4a89ce05b5c2f65292d852c
Evelyn Tavares <evelyn.tavares7@hotmail.com>
2024-06-28 13:22:09 -0600

    Adding Leaflet library, loading map and adding markers on map

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map;
+
+const getGeoLocation = function (position) {
+  const { latitude, longitude } = position.coords;
+
+  const coords = [latitude, longitude];
+  map = L.map('map').setView(coords, 14);
+
+  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  }).addTo(map);
+
+  map.on('click', addMarkerToMap);
+};
+
+const addMarkerToMap = function (mapEvent) {
+  const { lat, lng } = mapEvent.latlng;
+  const coords = [lat, lng];
+
+  const popupConfig = {
+    maxWidth: 250,
+    minWidth: 100,
+    autoClose: false,
+    closeOnClick: false,
+    className: 'running-popup',
+  };
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(L.popup(popupConfig))
+    .setPopupContent('Workout')
+    .openPopup();
+};
+
+navigator.geolocation.getCurrentPosition(getGeoLocation, function () {
+  alert('Unable to determine position');
+});
